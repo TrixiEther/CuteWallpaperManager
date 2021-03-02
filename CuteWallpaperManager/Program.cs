143c90645ec73e1d5befd28e43998b1b3376b78f
TrixiEther <trixiether@gmail.com>
2021-03-02 17:50:03 +0300

    init - proof of concept

---
+++ CuteWallpaperManager/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuteWallpaperManager
+{
+
+    static class Program
+    {
+        /// <summary>
+        /// set the parameter of system
+        /// </summary>
+        /// <param name="uAction"></param>
+        /// <param name="uParam"></param>
+        /// <param name="lpvParam"></param>
+        /// <param name="fuWinIni"></param>
+        /// <example></example>
+        /// <returns></returns>
+        
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        static void Main()
+        {
+#if DEBUG
+            WallpaperService ws = new WallpaperService();
+            ws.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new WallpaperService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ CuteWallpaperManager/WallpaperService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CuteWallpaperManager
+{
+
+    public enum UAction
+    {
+        /// <summary>
+        /// set the desktop background image
+        /// </summary>
+        SPI_SETDESKWALLPAPER = 0x0014,
+        /// <summary>
+        /// set the desktop background image
+        /// </summary>
+        SPI_GETDESKWALLPAPER = 0x0073,
+    }
+
+    public partial class WallpaperService : ServiceBase
+    {
+        string testPath1 = "C:/wallpapers/1.jpg";
+        string testPath2 = "C:/wallpapers/2.jpg";
+
+        bool useFirst = true;
+        TimerCallback tm;
+        Timer timer;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern int SystemParametersInfo(UAction uAction, int uParam, StringBuilder lpvParam, int fuWinIni);
+
+        public WallpaperService()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            tm = new TimerCallback(ChangeWallpaper);
+            timer = new Timer(tm, null, 0, 10000);
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void ChangeWallpaper(object obj)
+        {
+            if ( useFirst )
+            {
+                StringBuilder s = new StringBuilder(testPath1);
+                SystemParametersInfo(UAction.SPI_SETDESKWALLPAPER, 0, s, 0x2);
+                useFirst = false;
+            } else
+            {
+                StringBuilder s = new StringBuilder(testPath2);
+                SystemParametersInfo(UAction.SPI_SETDESKWALLPAPER, 0, s, 0x2);
+                useFirst = true;
+            }
+        }
+    }
+}
